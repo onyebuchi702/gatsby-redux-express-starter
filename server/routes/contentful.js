2bd47e57c4c3573469563d6f91a75e9406ffe8d4
onyebuchi702 <emechebeonyebuchi@mail.com>
2021-03-09 12:16:07 +0200

    used configs

--- server/routes/contentful.js
+++ server/routes/contentful.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
-// const config = require('config');
-// const CONTENTFUL = config.contentful;
-// const CONTENTFUL_BASE_URL = config.contentful_url;
+const config = require('../../config/gatsby-express.json');
+const CONTENTFUL = config.contentful;
+const CONTENTFUL_BASE_URL = config.contentful_url;
 
 router.get('/', async (req, res) => {
 	try {
 		const response = await axios
 			.get(
-				`${'https://cdn.contentful.com/'}spaces/${'kmyahsy08mbl'}/entries?access_token=${'GNluBnlJEpq0nfcjGMBPSY5ab3sjKuwYhd4H9YNtKFY'}`,
+				`${'https://cdn.contentful.com/'}spaces/${CONTENTFUL['spaceID']}/entries?access_token=${CONTENTFUL['accessToken']}`,
 				{}
 			)
 			.then((response) => {
